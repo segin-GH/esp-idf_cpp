be08d3e3115670a484c8e19e028cea8e15cd21dd
segin-GH <segin.bytes@gmail.com>
2022-11-04 15:48:23 +0530

    ,

--- TimeStamp/main/main.cpp
+++ TimeStamp/main/main.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
+#include <string>
 #include <ctime>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <sys/time.h>
-
 
 extern "C" {
     void app_main(void);
 }
 
 struct timeval tv;
+
+std::string getTimeStamp(struct timeval &_tv)
+{
+    gettimeofday(&tv, NULL);
+    std::string data = std::to_string(time(NULL)); 
+    return data;
+}
 
 //TODO timestamp 
 
@@ -21,11 +28,16 @@ void app_main(void)
     int err = settimeofday(&tv, NULL);
     if (err != 0)
         esp_restart();
+    
     while(true)
     {
-        gettimeofday(&tv, NULL);
-        std::cout << time(NULL) << std::endl;\
+        std::string data = getTimeStamp(tv);
+        std::string uartData = "luffy";
+        std::string newData = data + ":" + uartData;
+        std::cout << newData << std::endl;
         vTaskDelay(2000/portTICK_PERIOD_MS);
     }
+
+
 }
 
