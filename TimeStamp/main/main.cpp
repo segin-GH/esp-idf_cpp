54a641a1a3b21324946b66410b4ad9acd9fa14ba
segin-GH <segin.bytes@gmail.com>
2023-02-10 15:17:41 +0530

    .

--- TimeStamp/main/main.cpp
+++ TimeStamp/main/main.cpp
@@ -1,42 +1,57 @@
-#include <iostream>
+#include <esp_log.h>
 #include <string>
-#include <ctime>
+#include <sys/time.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
-#include <sys/time.h>
 
-extern "C" {
-    void app_main(void);
-}
-
-struct timeval tv;
-
-std::string getTimeStamp(struct timeval &_tv)
+class Time
 {
-    gettimeofday(&tv, NULL);
-    std::string data = std::to_string(time(NULL)); 
-    return data;
-}
+private:
+    struct timeval tv;
 
-//TODO timestamp 
+public:
+    std::string m_usec;
+    std::string m_sec;
 
-void app_main(void)
+public:
+    void setTime();
+    std::string getTime();
+};
+
+void Time::setTime()
 {
-    tv.tv_sec = 1666945715;
-    tv.tv_usec = 0;
-
+    tv.tv_sec = strtol(m_sec.c_str(), NULL, 10);
+    if(m_usec.length() != 0)
+        tv.tv_usec = strtol(m_usec.c_str(), NULL ,10);
     int err = settimeofday(&tv, NULL);
     if (err != 0)
-        esp_restart();
-    
-    while(true)
     {
-        std::string data = getTimeStamp(tv);
-	int64_t time_us = (int64_t)tv.tv_sec * 1000000L + (int64_t)tv.tv_usec;
-        std::string uartData = "luffy";
-        data = data + ":" + uartData;
-        std::cout << data << std::endl;
-        vTaskDelay(2000/portTICK_PERIOD_MS);
+        ESP_LOGE("TIME CONFIG", "Unable to set time");
     }
+}
 
+std::string Time::getTime()
+{
+    gettimeofday(&tv, NULL);
+    int64_t time_us = (int64_t)tv.tv_sec * 1000000L + (int64_t)tv.tv_usec;
+    char buf[50];
+    sprintf(buf, "ES1:%lld:", (unsigned long long)time_us);
+    return std::string(buf);
+}
+
+
+extern "C" void app_main(void)
+{
+    Time time;
+    time.m_sec = "1676022260";
+    time.m_usec = "0";
+    time.setTime();
+    
+    for(;;)
+    {
+        // Set the time to 1000000000 microseconds (1 second) from the epoch
+        std::string str = time.getTime();
+        ESP_LOGI("Time", "%s", str.c_str());
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+    }
 }
