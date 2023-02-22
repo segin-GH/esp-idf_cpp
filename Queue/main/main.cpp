d1679b36ed5c91ba585fdcf4ffb2025c53a37ba8
segin-GH <segin.bytes@gmail.com>
2023-02-22 12:40:11 +0530

    .

--- Queue/main/main.cpp
+++ Queue/main/main.cpp
@@ -4,23 +4,32 @@
 #include <freertos/task.h>
 #include <freertos/queue.h>
 
-xQueueHandle queue;
-int queueLen = 10;
+// xQueueHandle queue;
+// int queueLen = 10;
+
+// extern "C" void app_main(void)
+// {
+//     queue = xQueueCreate(queueLen, (sizeof(char) * 1000 ));
+
+//     for (;;)
+//     {
+//         char str[1000] ="abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
+//         xQueueSend(queue, str, portMAX_DELAY);
+
+//         char recvBuff[1000] = "";
+//         memset(recvBuff, 0 ,sizeof (1000));
+//         xQueueReceive(queue, &recvBuff, portMAX_DELAY);
+
+//         std::cout << recvBuff << std::endl;
+//         vTaskDelay(10/portTICK_PERIOD_MS);
+//     }
+// }
 
 extern "C" void app_main(void)
 {
-    queue = xQueueCreate(queueLen, (sizeof(char) * 1000 ));
-    
-    for (;;)
-    {
-        char str[1000] ="abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
-        xQueueSend(queue, str, portMAX_DELAY);
+    WORD_ALIGNED_ATTR char rx_buf[150] = "";
+    std::cout << "Len :: " << strlen(rx_buf) << std::endl;
 
-        char recvBuff[1000] = "";
-        memset(recvBuff, 0 ,sizeof (1000));
-        xQueueReceive(queue, &recvBuff, portMAX_DELAY);
-
-        std::cout << recvBuff << std::endl;
-        vTaskDelay(10/portTICK_PERIOD_MS);
-    }
+    sprintf(rx_buf, "oooooooooo");
+    std::cout << "Len :: " << strlen(rx_buf) << std::endl;
 }
